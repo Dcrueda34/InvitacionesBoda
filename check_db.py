fd6eedb2ebe423ab63451c1dbe8eaf3e3de07d8e
Diana  Rueda <112775120+Dcrueda34@users.noreply.github.com>
2024-09-03 17:12:39 -0500

    Delete check_db.py

--- check_db.py
+++
@@ -1,7 +0,0 @@
-from app import db, Confirmation
-
-# Consultar todas las confirmaciones
-confirmations = Confirmation.query.all()
-for confirmation in confirmations:
-    print(confirmation.phone_number, confirmation.name, confirmation.adults, confirmation.children, confirmation.choice)
-
